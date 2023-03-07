38861c53e9edaccb43b3eea4464519d90cbbc284
Swaraag <49351868+Swaraag@users.noreply.github.com>
2023-03-07 09:31:50 -0800

    improved join server page

--- client/Screens/Join.js
+++ client/Screens/Join.js
@@ -1,49 +1,31 @@
 import {
   StyleSheet,
-  Button,
   Text,
   View,
   ImageBackground,
   TouchableOpacity,
-  Touchable,
+  TextInput,
 } from 'react-native';
 import React, { useState } from 'react';
 
 function JoinScreen({ navigation }) {
+  const [code, onChangeCode] = useState('');
   return (
     <View style={styles.menuContainer}>
-      <ImageBackground
-        source={require('client/assets/menuBackground.png')}
-        resizeMode="cover"
-        style={styles.backgroundImage}
+      <View style={{ flex: 1, backgroundColor: 'transparent' }} />
+      <Text style={styles.titleText}>Join Server</Text>
+      <TextInput
+        style={styles.TextInput}
+        onChangeText={onChangeCode}
+        placeholder="XXXXXX"
+      ></TextInput>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Lobby')}
       >
-        <View style={{ flex: 1, backgroundColor: 'transparent' }} />
-        <Text style={styles.titleText}>Join Page</Text>
-        <Text style={styles.lakesideText}>(Lakeside Edition)</Text>
-        <View style={styles.buttonsContainer}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => navigation.navigate('Lobby')}
-          >
-            <Text style={styles.touchableButton}>Join</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => navigation.navigate('Lobby')}
-          >
-            <Text style={styles.touchableButton}>Public</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => navigation.navigate('Lobby')}
-          >
-            <Text style={styles.touchableButton}>Create</Text>
-          </TouchableOpacity>
-          <View style={styles.emptyTouchableView}></View>
-        </View>
-      </ImageBackground>
+        <Text style={styles.touchableButton}>Join</Text>
+      </TouchableOpacity>
+      <View style={styles.emptyTouchableView}></View>
     </View>
   );
 }
@@ -63,33 +45,13 @@ const styles = StyleSheet.create({
     flex: 0.5,
     letterSpacing: 3,
   },
-  buttonsContainer: {
-    color: '#505050',
-    flex: 2,
-  },
-  backgroundImage: {
-    flex: 1,
-    justifyContent: 'center',
-    width: '100%',
-    height: '100%',
-  },
   button: {
-    alignItems: 'center',
     backgroundColor: '#BDC9C9',
-    flex: 1,
-    marginTop: 20,
-    marginBottom: 10,
-    marginLeft: 70,
-    marginRight: 70,
+    paddingLeft: 40,
+    paddingRight: 40,
     paddingTop: 15,
     paddingBottom: 15,
     borderRadius: 20,
-  },
-  lakesideText: {
-    color: 'black',
-    flex: 2,
-    fontSize: 20,
-    textAlign: 'center',
   },
   emptyTouchableView: {
     flex: 4,
@@ -97,7 +59,12 @@ const styles = StyleSheet.create({
   touchableButton: {
     color: 'black',
     fontSize: 17,
-    justifyContent: 'center',
+  },
+  TextInput: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
 
