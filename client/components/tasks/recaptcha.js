6cc9b12d94952f9b9257d13e0e85b5bc591c451e
William Y. Feng <womogenes2@gmail.com>
2023-04-04 09:17:33 -0700

    Reformat recaptcha.js

--- client/components/tasks/recaptcha.js
+++ client/components/tasks/recaptcha.js
@@ -1,53 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useRef, useEffect } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-} from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 import Recaptcha from 'react-native-recaptcha-that-works';
 
 function CaptchaTask(props) {
-    const recaptcha = useRef();
-    
-    const send = () => {
-        console.log('send!');
-        recaptcha.current.open();
-    }
+  const recaptcha = useRef();
 
-    const onVerify = token => {
-        props.complete('reCaptcha');
-        console.log('success!', token);
-    }
+  const send = () => {
+    console.log('send!');
+    recaptcha.current.open();
+  };
 
-    const onExpire = () => {
-        console.warn('expired');
-    }
+  const onVerify = (token) => {
+    props.complete('reCaptcha');
+    console.log('success!', token);
+  };
 
-    useEffect(() => {
-      if(props.active == true){
-        send();
-      }
-    }, [props.active]);
+  const onExpire = () => {
+    console.warn('expired');
+  };
+
+  useEffect(() => {
+    if (props.active == true) {
+      send();
+    }
+  }, [props.active]);
 
   return (
     <View>
-        <Recaptcha
-            ref={recaptcha}
-            siteKey="6Le7-FciAAAAACnKxo3JECtz17LYl2VjJgC17ydG"
-            baseUrl="http://127.0.0.1"
-            onVerify={onVerify}
-            onExpire={onExpire}
-            size="normal"
-        />
+      <Recaptcha
+        ref={recaptcha}
+        siteKey="6Le7-FciAAAAACnKxo3JECtz17LYl2VjJgC17ydG"
+        baseUrl="http://127.0.0.1"
+        onVerify={onVerify}
+        onExpire={onExpire}
+        size="normal"
+      />
     </View>
   );
 }
 
 export default CaptchaTask;
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
