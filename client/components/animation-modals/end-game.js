3b616512ff815dffe50f43bf281e1f8edb00111d
William Y. Feng <womogenes2@gmail.com>
2023-06-02 07:34:15 -0700

    Fix some styling

--- client/components/animation-modals/end-game.js
+++ client/components/animation-modals/end-game.js
@@ -1,4 +1,11 @@
-import { StyleSheet, View, Modal, TouchableOpacity, Text, ScrollView } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Modal,
+  TouchableOpacity,
+  Text,
+  ScrollView,
+} from 'react-native';
 import { ProfileIcon } from '../profile-icon';
 import CustomText from '../text';
 import { useState, useEffect } from 'react';
@@ -16,44 +23,73 @@ export const EndGame = (props) => {
     setIsVisible(Object.keys(props.team).length > 0);
   }, [props.team]);
 
-  function mapPlayers(){
-    return(
-      props.players?.map((player) => {
-        return(
-          <View key={player?.sessionId} style={[player?.isAlive? {opacity: 1} : {opacity: 0.4}, styles.listItem]}>
-            <View style={[styles.profileContainer,
-              props.team && props.team === 'impostor' ?
-                player?.isImpostor? [{shadowOpacity: 0.9}, {shadowColor: '#fff700'}, {shadowRadius: 16}]
-                : {shadowOpacity: 0}
-              : !player?.isImpostor? [{shadowOpacity: 0.9}, {shadowColor: '#fff700'}, {shadowRadius: 16}]
-                : {shadowOpacity: 0}]}
-            >
-              <ProfileIcon player={player} size={50} isImpostor={true} myId={props.myId}/>
-            </View>
-            <View style={styles.username}>
-              <CustomText textSize={30} textColor={player?.isImpostor? 'red': 'black'}>{player.username}</CustomText>
-            </View>
+  function mapPlayers() {
+    return props.players?.map((player) => {
+      return (
+        <View
+          key={player?.sessionId}
+          style={[
+            player?.isAlive ? { opacity: 1 } : { opacity: 0.4 },
+            styles.listItem,
+          ]}
+        >
+          <View
+            style={[
+              styles.profileContainer,
+              props.team && props.team === 'impostor'
+                ? player?.isImpostor
+                  ? [
+                      { shadowOpacity: 0.9 },
+                      { shadowColor: '#fff700' },
+                      { shadowRadius: 16 },
+                    ]
+                  : { shadowOpacity: 0 }
+                : !player?.isImpostor
+                ? [
+                    { shadowOpacity: 0.9 },
+                    { shadowColor: '#fff700' },
+                    { shadowRadius: 16 },
+                  ]
+                : { shadowOpacity: 0 },
+            ]}
+          >
+            <ProfileIcon
+              player={player}
+              size={50}
+              isImpostor={true}
+              myId={props.myId}
+            />
           </View>
-        );
-      })
-    )
+          <View style={styles.username}>
+            <CustomText
+              textSize={30}
+              textColor={player?.isImpostor ? 'red' : 'black'}
+            >
+              {player.username}
+            </CustomText>
+          </View>
+        </View>
+      );
+    });
   }
   return (
-    <AnimationModal isVisible={isVisible} height={'80%'} onClose={props.onClose}>
+    <AnimationModal
+      isVisible={isVisible}
+      height={'80%'}
+      onClose={props.onClose}
+    >
       <View style={styles.container}>
         <View style={styles.fit}>
           <CustomText textSize={80} textColor={'red'} textAlign={'center'}>
             <Text>Game Over</Text>
           </CustomText>
           <CustomText textSize={60} textColor={'#fff'} textAlign={'center'}>
-            <Text>The {props.team}</Text>            
+            <Text>The {props.team}</Text>
           </CustomText>
           <CustomText textSize={50} textColor={'#888'} textAlign={'center'}>
             <Text>team has won!</Text>
           </CustomText>
-          <ScrollView style={styles.profileList}>
-            {mapPlayers()}
-          </ScrollView>
+          <ScrollView style={styles.profileList}>{mapPlayers()}</ScrollView>
         </View>
       </View>
     </AnimationModal>
@@ -75,7 +111,7 @@ const styles = StyleSheet.create({
     borderRadius: 100,
     width: 50,
     height: 50,
-    backgroundColor: 'red'
+    backgroundColor: 'red',
   },
   fit: {
     flex: 1,
--- client/components/animation-modals/start-game.js
+++ client/components/animation-modals/start-game.js
@@ -1,4 +1,11 @@
-import { StyleSheet, View, Modal, TouchableOpacity, Text, ScrollView } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Modal,
+  TouchableOpacity,
+  Text,
+  ScrollView,
+} from 'react-native';
 import { ProfileIcon } from '../profile-icon';
 import CustomText from '../text';
 import { useState, useEffect } from 'react';
@@ -9,46 +16,68 @@ export const StartGame = (props) => {
     props: playerId (Colyseus state object of dead player)
   */
 
-
   function mapPlayers() {
-    return(
-      props.isImpostor?
-      props.players?.map((player) => {
-        return(
-          <View key={player?.sessionId} style={[player?.isImpostor? {opacity: 1} : {opacity: 0.4}, styles.listItem]}>
-            <ProfileIcon player={player} size={50} isImpostor={props.isImpostor} myId={props.sessionId}/>
-            <View style={styles.username}>
-              <CustomText textSize={30} textColor={player?.isImpostor? 'red': 'black'}>{player.username}</CustomText>
+    return props.isImpostor
+      ? props.players?.map((player) => {
+          return (
+            <View
+              key={player?.sessionId}
+              style={[
+                player?.isImpostor ? { opacity: 1 } : { opacity: 0.4 },
+                styles.listItem,
+              ]}
+            >
+              <ProfileIcon
+                player={player}
+                size={50}
+                isImpostor={props.isImpostor}
+                myId={props.sessionId}
+              />
+              <View style={styles.username}>
+                <CustomText
+                  textSize={30}
+                  textColor={player?.isImpostor ? 'red' : 'black'}
+                >
+                  {player.username}
+                </CustomText>
+              </View>
             </View>
-          </View>
-        );
-      })
-      :
-      props.players?.map((player) => {
-        return(
-          <View key={player?.sessionId} style={[styles.listItem]}>
-            <ProfileIcon player={player} size={50} myId={props.sessionId}/>
-            <View style={styles.username}>
-              <CustomText textSize={30}>{player.username}</CustomText>
+          );
+        })
+      : props.players?.map((player) => {
+          return (
+            <View key={player?.sessionId} style={[styles.listItem]}>
+              <ProfileIcon player={player} size={50} myId={props.sessionId} />
+              <View style={styles.username}>
+                <CustomText textSize={30}>{player.username}</CustomText>
+              </View>
             </View>
-          </View>
-        );
-      })
-    )
+          );
+        });
   }
 
   return (
-    <AnimationModal isVisible={props.isVisible} height={'80%'} onClose={props.onClose}>
+    <AnimationModal
+      isVisible={props.isVisible}
+      height={'80%'}
+      onClose={props.onClose}
+    >
       <View style={styles.container}>
-        <CustomText textSize={60} centerText={true} textColor={props.isImpostor? 'red' : 'white'}>
-          {props.isImpostor? <Text>Impostor</Text> : <Text>Crewmate</Text>}
+        <CustomText
+          textSize={60}
+          centerText={true}
+          textColor={props.isImpostor ? 'red' : 'white'}
+        >
+          {props.isImpostor ? <Text>Impostor</Text> : <Text>Crewmate</Text>}
         </CustomText>
         <CustomText textSize={30} textColor={'#9c9c9c'} centerText={true}>
-          {props.isImpostor? <Text>Be the last standing!</Text> : <Text>There is an impostor among us!</Text>}
+          {props.isImpostor ? (
+            <Text>Be the last standing!</Text>
+          ) : (
+            <Text>There is an impostor among us!</Text>
+          )}
         </CustomText>
-        <ScrollView style={styles.profileList}>
-          {mapPlayers()}
-        </ScrollView>
+        <ScrollView style={styles.profileList}>{mapPlayers()}</ScrollView>
       </View>
     </AnimationModal>
   );
@@ -57,7 +86,7 @@ export const StartGame = (props) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
+    padding: 25,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#000',
@@ -65,17 +94,16 @@ const styles = StyleSheet.create({
   },
   profileList: {
     flexDirecion: 'row',
-    width: '80%',
-    height: '80%',
+    width: '100%',
     padding: 5,
-    margin: 5,
+    marginTop: 20,
     backgroundColor: '#FFFFFF',
-    borderRadius: 20,
+    borderRadius: 15,
   },
   listItem: {
     margin: 5,
     flexDirection: 'row',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   username: {
     margin: 5,
